9389d4b2e7600406c7b3ea0f8b2f37b151959193
RDK2305 <rudrakshkharadi53@gmail.com>
2026-04-05 14:30:00 +0530

    add axios api service with interceptors

---
+++ frontend/src/services/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+API.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error)
+);
+
+export default API;
